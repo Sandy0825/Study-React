8d0c0e0171750186bf4799a8c83fc88a9cf96ae0
Sandy0825 <ddori825@gmail.com>
2024-03-06 11:00:16 +0900

    리액트 라우터

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,38 +2,71 @@ import {Nav, Navbar, Container} from 'react-bootstrap';
 import data from './data';
 import './App.css';
 import { useState } from 'react';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detail'
 
 
 function App() {
   let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/about')}}>Features</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/detail')}}>Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      <div className='main-bg'></div>
-
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map(function(a,i){
-              return(
-                <Card shoes={shoes[i]} i={i+1}/>
-              )
-            })
-          }
-          
-        </div>
-      </div>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg'></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map(function(a,i){
+                    return(
+                      <Card shoes={shoes[i]} i={i+1}/>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </>
+        } />
+        <Route path="/about" element={<About />}>
+          <Route path="company" element={<div>회사소개</div>} />
+          <Route path="member" element={<div>멤버들소개</div>} />
+        </Route>
+        <Route path='/event' element={<Event />}>
+          <Route path='one' element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path='two' element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+      </Routes>
     </div>
+  );
+}
+function Event() {
+  return (
+    <>
+      <h3>오늘의 이벤트</h3>
+      <Outlet></Outlet>
+    </>
+  )
+}
+function About() {
+  return (
+    <>
+      <div>About 예시페이지 제목</div>
+      <Outlet></Outlet>
+    </>
   );
 }
 
