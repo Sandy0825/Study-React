05df7bf9676b3e887d63d1607b1ccc0b68c4ca2e
Sandy0825 <ddori825@gmail.com>
2024-03-22 14:28:46 +0900

    Lifecycle과 useEffect

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail(props) {
     let { id } = useParams();
     let item = props.shoes.find((shoe) => shoe.id == id);
+
+    //2초 후 사라지는 박스 함수 만들기
+    const [warning, setWarning] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {setWarning(false)}, 2000)
+    }, []);
+
+    // input에 숫자 아닌 문자 넣으면 알럿뜨게 만들기
+    // isNaN('숫자')이면 false isNaN('문자')이면 true 나옴
+    const [type, setType] = useState('');
+    useEffect(() => {
+        if(isNaN(type) == true){
+            alert('돈 두댓!')
+        }
+    }, [type])
+    
+
     return (
         <div className="container">
+            {warning ? <div className='alert alert-warning'>2초후 사라짐</div> : null}
+            
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -16,6 +36,7 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
+            <input onChange={(e) => {setType(e.target.value)}} />
         </div> 
     )
 }
